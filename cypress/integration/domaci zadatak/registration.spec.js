e5da6dfa968c354f4fee7d4e52202b6db4d48b11
Danijela <danijelaalukovic@gmail.com>
2021-07-28 19:04:52 +0200

    commit2

--- cypress/integration/domaci zadatak/registration.spec.js
+++ cypress/integration/domaci zadatak/registration.spec.js
@@ -37,3 +37,67 @@ describe("Login", () => {
         cy.get('.btn-custom').click()
     })
 })
+
+describe("Registration - negative tests", () => {
+    const user = {
+        name: faker.name.firstName(),
+        lastname: faker.name.lastName(),
+        email: faker.internet.exampleEmail(),
+        password: faker.internet.password()
+    }
+    it ('Register new user without first name', () => {
+        cy.visit('/')
+        cy.get('.nav-link').eq(2).click()
+        cy.get('#last-name').type(user.lastname)
+        cy.get('#email').type(user.email)
+        cy.get('#password').type(user.password)
+        cy.get('#password-confirmation').type(user.password)
+        cy.get('.form-check-input').click()
+        cy.get('.btn-custom').click()
+    })
+    it ('Register new user without last name', () => {
+        cy.visit('/')
+        cy.get('.nav-link').eq(2).click()
+        cy.get('#first-name').type(user.name)
+        cy.get('#email').type(user.email)
+        cy.get('#password').type(user.password)
+        cy.get('#password-confirmation').type(user.password)
+        cy.get('.form-check-input').click()
+        cy.get('.btn-custom').click()
+    })
+    it ('Register new user with wrong format of email', () => {
+        cy.visit('/')
+        cy.get('.nav-link').eq(2).click()
+        cy.get('#first-name').type(user.name)
+        cy.get('#last-name').type(user.lastname)
+        cy.get('#email').type('daaajkfljs@ldfaj')
+        cy.get('#password').type(user.password)
+        cy.get('#password-confirmation').type(user.password)
+        cy.get('.form-check-input').click()
+        cy.get('.btn-custom').click()
+        cy.get('.alert').should('be.visible')
+    })
+    it ('Register new user with password less than 8 characters', () => {
+        cy.visit('/')
+        cy.get('.nav-link').eq(2).click()
+        cy.get('#first-name').type(user.name)
+        cy.get('#last-name').type(user.lastname)
+        cy.get('#email').type(user.email)
+        cy.get('#password').type('12345')
+        cy.get('#password-confirmation').type('12345')
+        cy.get('.form-check-input').click()
+        cy.get('.btn-custom').click()
+        cy.get('.alert').should('be.visible')
+    })
+    it ('Register without checking terms and conditions', () => {
+        cy.visit('/')
+        cy.get('.nav-link').eq(2).click()
+        cy.get('#first-name').type(user.name)
+        cy.get('#last-name').type(user.lastname)
+        cy.get('#email').type(user.email)
+        cy.get('#password').type(user.password)
+        cy.get('#password-confirmation').type(user.password)
+        cy.get('.btn-custom').click()
+        cy.get('.alert').should('be.visible')
+    })
+})
